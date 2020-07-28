5cccdccfb27526e4459f5fa950c58de08d4e350f
Aditya Pratap <adityapratap7344@gmail.com>
2020-07-28 18:43:08 +0530

    - added text input and button

--- App.tsx
+++ App.tsx
@@ -1,21 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Course Goal"
+        />
+        <Button
+          title="ADD"
+          onPress={() => { }}
+        />
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    padding: 50,
+  },
+  header: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  textInput: {
+    width: '80%',
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    padding: 10,
+    marginVertical: 20,
   },
 });
